7ee4abc998f58e42e0a1d340873125bc43e15fea
44uwu <aimeric.rouyer@epitech.eu>
2024-04-24 16:49:01 +0200

    :bug: fix a crash under specific condition

--- src/parse_cmd.c
+++ src/parse_cmd.c
@@ -9,12 +9,6 @@
 
 void parse_cmd(int argc, char **argv, ftrace_struct_t *args)
 {
-    int opt;
-
-    for (opt = 1; opt < argc; opt++) {
-        if (argv[opt][0] != '-' && args->given_cmd.cmd == NULL) {
-            args->given_cmd.cmd = argv[opt];
-            args->given_cmd.args = &argv[opt];
-        }
-    }
+    args->given_cmd.cmd = argv[1];
+    args->given_cmd.args = &argv[1];
 }
--- src/print_syscall_info.c
+++ src/print_syscall_info.c
@@ -8,37 +8,21 @@
 #include "ftrace.h"
 #include "syscall.h"
 
-void print_syscall_info_tree(struct user_regs_struct *regs)
+static void print_reg(unsigned long long int reg)
 {
-    if (table[regs->orig_rax].nb_args == 5)
-        printf("0x%llx, 0x%llx, 0x%llx, 0x%llx, 0x%llx", regs->rdi,
-            regs->rsi, regs->rdx, regs->r10, regs->r8);
-    else if (table[regs->orig_rax].nb_args == 6)
-        printf("0x%llx, 0x%llx, 0x%llx, 0x%llx, 0x%llx, 0x%llx",
-            regs->rdi, regs->rsi, regs->rdx, regs->r10, regs->r8, regs->r9);
-    else
-        printf("Unknown number of arguments");
-}
-
-void print_syscall_info_two(struct user_regs_struct *regs)
-{
-    if (table[regs->orig_rax].nb_args == 3)
-        printf("0x%lld, 0x%llx, 0x%llx", regs->rdi, regs->rsi, regs->rdx);
-    else if (table[regs->orig_rax].nb_args == 4)
-        printf("0x%llx, 0x%llx, 0x%llx, 0x%llx",
-            regs->rdi, regs->rsi, regs->rdx, regs->r10);
-    else
-        print_syscall_info_tree(regs);
+    printf("0x%llx", reg);
 }
 
 void print_syscall_info(struct user_regs_struct *regs)
 {
+    unsigned long long int regs_table[] = {
+        regs->rdi, regs->rsi, regs->rdx, regs->r10, regs->r8, regs->r9
+    };
     printf("%s(", table[regs->orig_rax].name);
-    if (table[regs->orig_rax].nb_args == 1)
-        printf("%c", (char)regs->rdi);
-    else if (table[regs->orig_rax].nb_args == 2)
-        printf("0x%llx, 0x%llx", regs->rdi, regs->rsi);
-    else
-        print_syscall_info_two(regs);
+    for (int i = 0; i < table[regs->orig_rax].nb_args; i++) {
+        if (i != 0)
+            printf(", ");
+        print_reg(regs_table[i]);
+    }
     printf(")\t = %lld\n", regs->rax);
 }
